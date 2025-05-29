77c706b146f40d1e408322b22002f11bec7d7319
junex10 <43711689+junex10@users.noreply.github.com>
2025-05-28 22:24:54 -0400

    Integrating leaflet

--- app/src/map/map.tsx
+++ app/src/map/map.tsx
@@ -1,15 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Button, ActivityIndicator, Alert } from 'react-native';
 import { Link } from 'expo-router';
+import { Asset } from "expo-asset";
+import * as FileSystem from "expo-file-system";
+import { LeafletView } from 'react-native-leaflet-view';
 
-function Map() {
+const DEFAULT_LOCATION = {
+  latitude: -23.5489,
+  longitude: -46.6388
+}
+
+const Map: React.FC = () => {
+    const [webViewContent, setWebViewContent] = useState<string | null>(null);
+  useEffect(() => {
+    let isMounted = true;
+
+    const loadHtml = async () => {
+      try {
+        const path = require("./../../../assets/leaflet.html");
+        const asset = Asset.fromModule(path);
+        await asset.downloadAsync();
+        const htmlContent = await FileSystem.readAsStringAsync(asset.localUri!);
+
+        if (isMounted) {
+          setWebViewContent(htmlContent);
+        }
+      } catch (error) {
+        Alert.alert('Error loading HTML', JSON.stringify(error));
+        console.error('Error loading HTML:', error);
+      }
+    };
+
+    loadHtml();
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  if (!webViewContent) {
+    return <ActivityIndicator size="large" />
+  }
     return (
       <>
-        <Text>This is the map</Text>
+        {/*<Text>This is the map</Text>
         <Link href="./create-account" style={styles.button}>
           Go back to Home screen!
         </Link>
-        <StatusBar style="auto" />
+        <StatusBar style="auto" />*/}
+        <LeafletView
+          source={{ html: webViewContent }}
+          mapCenterPosition={{
+            lat: DEFAULT_LOCATION.latitude,
+            lng: DEFAULT_LOCATION.longitude,
+          }}
+        />
       </>
     );
   }
